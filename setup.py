da5df1f9ad303ae3b739584a23bd3adb615ac2f5
alex_kavanaugh <none@none>
2013-08-23 16:17:13 -0700

    readme fix

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='django-ewiz',
-    version='1.1.14',
+    version='1.1.15',
     author='Alex Kavanaugh',
     author_email='kavanaugh.development@outlook.com',
     packages=['django_ewiz', 'django_ewiz/djangotoolbox', 'django_ewiz/djangotoolbox/db'],
     url='https://bitbucket.org/alex_kavanaugh/django-ewiz/',
     license='GNU LGPL (http://www.gnu.org/licenses/lgpl.html)',
     description="A non-relational Django database backend that utilizes EnterpriseWizard's REST interface.",
-    long_description="See 'README.rst'.",
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django>=1.5.2",
     ],
