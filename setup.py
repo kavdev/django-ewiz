89ebea3f53e875064ce4c9cadf2c932335fe02ef
alex_kavanaugh <none@none>
2013-08-23 16:01:05 -0700

    Loaded full package.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://bitbucket.org/alex_kavanaugh/django-ewiz/',
     license='GNU LGPL',
     description="A non-relational Django database backend that utilizes EnterpriseWizard's REST interface.",
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django>=1.5.2",
     ],
