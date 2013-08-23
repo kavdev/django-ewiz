8076e6217896559e00eb0d8cd9421fc0aa307a1c
alex_kavanaugh <none@none>
2013-08-23 16:08:06 -0700

    manifest bug

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='django-ewiz',
-    version='1.1.12',
+    version='1.1.13',
     author='Alex Kavanaugh',
     author_email='kavanaugh.development@outlook.com',
     packages=['django_ewiz', 'django_ewiz/djangotoolbox', 'django_ewiz/djangotoolbox/db'],
     url='https://bitbucket.org/alex_kavanaugh/django-ewiz/',
-    license='GNU LGPL',
+    license='GNU LGPL (http://www.gnu.org/licenses/lgpl.html)',
     description="A non-relational Django database backend that utilizes EnterpriseWizard's REST interface.",
     long_description=open('README.rst').read(),
     install_requires=[
